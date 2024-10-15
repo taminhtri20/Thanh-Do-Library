af35909ba281decfc9ab5d6ec4fa15103470be6d
taminhtri20 <taminhtri20@gmail.com>
2024-10-15 20:08:34 +0700

    first commit

---
+++ src/main/java/org/example/thanhdolibrary/ThanhDoLibraryApplication.java
@@ -0,0 +1,13 @@
+package org.example.thanhdolibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThanhDoLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThanhDoLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/thanhdolibrary/ThanhDoLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.thanhdolibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThanhDoLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
